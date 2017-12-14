0ff2548f969912e03b3a10458d36181ef0b8e9ae
Gildardo Rosas <gildardo.rosas96@gmail.com>
2017-12-13 19:42:21 -0800

    Obtenemos todos los comentarios del último post de una página

--- asoc/asoc.py
+++ asoc/asoc.py
@@ -5,6 +5,10 @@
 """
 
 import os, time
+import facebook
+import json
+import requests
+import urllib.request
 import sqlite3
 from flask import Flask, request, session, g, jsonify,redirect, url_for, abort, \
         render_template, flash
@@ -63,3 +67,46 @@ def add_numbers():
 @app.route('/')
 def index():
     return render_template('base.html')
+
+def get_page_comments(url, limit=400):
+    """Encuentra los comentarios del último post de la página de Facebook.
+       
+       Los url's por el momento se limitan a tener un solo slash. Por ejemplo:
+
+       https://www.facebook.com/NFL/
+
+       De esa manera con una simple expresión regular se obtiene el object_id,
+       el cual en este caso en particular es NFL.
+    """
+
+    messages = []
+
+    ACCESS_TOKEN = 'EAACEdEose0cBAO9f0XCyjPTNOBgx4wfUBzkLCDwAbZAYNnrLJ9B5FKLrZBOD50XoNVur4NHbUjnm1bmZAYLx0hZCvoZC9WNKVgf1NdYH5bdetUJ20XVKIDyZBIh4FZAD2enGh7kcUE4wntZAsUwZAli2W2tjoFs0FmMLGVT8b6HZAGqaGJB72sPzbp0cL8bw6PILEHk1XDRYF6ZBgZDZD'
+    g = facebook.GraphAPI(ACCESS_TOKEN)
+    object_id = url.rsplit('/')[3]
+    posts = g.get_connections(object_id, 'posts')
+    last_post = posts["data"][0]
+    last_post_id = last_post["id"]
+    comments = g.get_object(last_post_id, fields="comments")
+    
+    # Adjuntamos la primeros comentarios.
+
+    for comment in comments['comments']['data']:
+        message = comment['message']
+        if message:
+            messages.append(message)
+
+    # Si existen más comentarios tendremos que agregarlos también.
+
+    while len(messages) < limit and comments['comments']['paging'].get('next'):
+        _url = comments['comments']['paging']['next']
+        with urllib.request.urlopen(_url) as response:
+            p = response.read().decode('utf-8')
+            posts = json.loads(p)
+
+        for comment in posts['data']:
+            message = comment['message']
+            if message:
+                messages.append(message)
+
+    return messages # Caso de prueba: get_page_comments('https://www.facebook.com/animalsinrandomplaces/')
